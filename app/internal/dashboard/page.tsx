7c589c4e18b52eb3d02dfa38cbbea5790433f5d6
Gabriel Cavalcante <ggc.gabrielg@hotmail.com>
2023-09-08 16:03:54 -0300

    Creating pages to develop  dashboard

---
+++ app/internal/dashboard/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+export default function Home() {
+    return (
+        <div>
+            <main>Created</main>
+        </div>
+    )
+}
--- app/internal/layout.tsx
+++ app/internal/layout.tsx
@@ -1,28 +1,23 @@
-'use client';
 import Sidebar from '@/components/Sidebar/Sidebar';
 import React from 'react';
 import styles from './layout.module.scss';
-import { QueryClient, QueryClientProvider } from 'react-query';
 
 export interface IInternalLayout {
   children: React.ReactNode;
 }
 
 const InternalLayout: React.FC<IInternalLayout> = ({ children }) => {
-  const queryClient = new QueryClient();
   return (
     <>
-      <QueryClientProvider client={queryClient}>
-        <div className={`main flex flex-row h-full w-full`}>
-          <Sidebar />
+      <div className={`main flex flex-row h-full w-full`}>
+        <Sidebar />
 
-          <main
-            className={`p-8 flex-grow bg-slate-100 overflow-auto ${styles.main}`}
-          >
-            {children}
-          </main>
-        </div>
-      </QueryClientProvider>
+        <main
+          className={`p-8 flex-grow bg-slate-100 overflow-auto ${styles.main}`}
+        >
+          {children}
+        </main>
+      </div>
     </>
   );
 };
