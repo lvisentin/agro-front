5877baa9354f9969344a93050ee5131904620a7a
lvisentin <lvise.batista@gmail.com>
2023-09-11 21:10:14 -0300

    documents v1

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
-const path = require('path')
- 
+const path = require("path");
+
 module.exports = {
   env: {
     VERCEL_API_URL: process.env.VERCEL_API_URL,
   },
+  images: {
+    domains: ["placehold.it"],
+  },
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   parameters: {
     nextjs: {
       appDirectory: true,
       navigation: {
-        pathname: '/internal/dashboard,',
+        pathname: "/internal/dashboard,",
       },
     },
   },
-}
+};
