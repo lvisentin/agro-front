041c3654244274a6990078ca4f3427ed0fd3f654
Gabriel Cavalcante <ggc.gabrielg@hotmail.com>
2023-10-24 13:57:56 -0300

    feat: saving page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,13 +1,39 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import Link from 'next/link';
 
 export default function NotFound() {
-  const { push } = useRouter();
-  useEffect(() => {
-    push('/login');
-  }, []);
+  return <>
+    <div
+      className="
+        flex
+        items-center
+        justify-center
+        w-screen
+        h-screen
+        bg-white
+      "
+    >
+      <div className="px-40 py-20 bg-green-100 rounded-md shadow-xl">
+        <div className="flex flex-col items-center">
+          <h1 className="font-bold text-9xl">404</h1>
 
-  return <></>;
+          <h6 className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
+            <span className="text-red-500">Oops!</span> Página não encontrada
+          </h6>
+
+          <p className="mb-8 text-center text-gray-500 md:text-lg">
+            Parece que esse conteúdo não existe mais.
+          </p>
+
+          <Link
+            href="/"
+            className="px-6 py-2 text-sm font-semibold text-green-800 bg-green-100"
+          >
+            Voltar para home
+          </Link>
+        </div>
+      </div>
+    </div>
+  </>;
 }
