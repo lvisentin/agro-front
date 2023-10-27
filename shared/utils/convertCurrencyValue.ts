643f73802ca55bf83106ffa95463e96e61ba743e
lvisentin <lvise.batista@gmail.com>
2023-10-27 16:15:30 -0300

    fix: fix converted value

--- shared/utils/convertCurrencyValue.ts
+++ shared/utils/convertCurrencyValue.ts
@@ -1,12 +1,21 @@
 export default function convertCurrencyValue(value: string) {
-  let formattedValue = ''
+  let formattedValue = '';
+  let fullvalue = '';
+
+  if (value.includes('.')) {
+    const splitted = value.split('.');
+    for (let val of splitted) {
+      fullvalue += val;
+    }
+
+    value = fullvalue;
+  }
 
   if (value.includes(',')) {
     const splitted = value.split(',');
     formattedValue = `${splitted[0]}.${splitted[1]}`;
+    return Number(formattedValue);
   }
 
-  formattedValue = value
-
-  return Number(formattedValue)
+  return Number(value);
 }
