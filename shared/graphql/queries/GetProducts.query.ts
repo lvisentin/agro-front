ee61c2ad80ca59be06ccf7703511509d4fb8f2fb
Gabriel Cavalcante <ggc.gabrielg@hotmail.com>
2023-11-21 06:04:04 -0300

    feat: search by name on supply page

--- shared/graphql/queries/GetProducts.query.ts
+++ shared/graphql/queries/GetProducts.query.ts
@@ -1,8 +1,8 @@
 import { gql } from '@apollo/client';
 
 export const GetProductsQuery = gql`
-  query GetProducts($propertyId: Int) {
-    products(propertyId: $propertyId) {
+  query GetProducts($propertyId: Int, $name: String) {
+    products(propertyId: $propertyId, name: $name) {
       id
       category {
         id
