acdccbc5c6867a19bc4929ef47235e3337af1602
Gabriel Cavalcante <ggc.gabrielg@hotmail.com>
2023-11-21 06:04:04 -0300

    feat: search by name on supply page

--- app/internal/supply/page.tsx
+++ app/internal/supply/page.tsx
@@ -3,6 +3,8 @@
 import DataTable from '@/components/DataTable/DataTable';
 import NoData from '@/components/NoData/NoData';
 import PrimaryButton from '@/components/PrimaryButton/PrimaryButton';
+import SecondaryButton from '@/components/SecondaryButton/SecondaryButton';
+import TextField from '@/components/TextField/TextField';
 import { PageRoutes } from '@/shared/enums/PageRoutes';
 import { DeleteProductMutation } from '@/shared/graphql/mutations/DeleteProduct.mutation';
 import { GetProductsQuery } from '@/shared/graphql/queries/GetProducts.query';
@@ -12,11 +14,12 @@ import { useMutation, useQuery } from '@apollo/client';
 import { faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useRouter } from 'next/navigation';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 
 function SupplyPage() {
   const { push } = useRouter();
+  const [selectedProducty, setSelectedProducty] = useState('');
 
   const {
     loading,
@@ -30,10 +33,6 @@ function SupplyPage() {
   );
 
   const columns = [
-    // {
-    //   field: 'code',
-    //   name: 'Código',
-    // },
     {
       field: 'name',
       name: 'Nome',
@@ -78,6 +77,27 @@ function SupplyPage() {
     refetch();
   }, []);
 
+  useEffect(() => {
+    let timeoutId: any;
+
+    if (!selectedProducty) {
+      refetch({ name: undefined });
+      return;
+    }
+
+    timeoutId = setTimeout(() => {
+      refetch({
+        name: selectedProducty,
+      });
+    }, 200);
+
+    return () => clearTimeout(timeoutId);
+  }, [selectedProducty]);
+
+  function clearFilter() {
+    setSelectedProducty('');
+  }
+
   if (loading || deleteLoading) {
     return <span className="loading loading-spinner loading-lg"></span>;
   }
@@ -96,6 +116,28 @@ function SupplyPage() {
             <FontAwesomeIcon icon={faPlus} />
             Novo produto
           </PrimaryButton>
+        </div>
+
+        <div className="filter">
+          <div className="flex items-center gap-4">
+            <TextField
+              value={selectedProducty}
+              disabled={loading}
+              onChange={(e) => {
+                setSelectedProducty(e.target.value);
+              }}
+              name="property"
+              placeholder="Filtrar por nome"
+              label="Nome"
+            />
+            <SecondaryButton
+              type="button"
+              onClick={clearFilter}
+              className="mt-4"
+            >
+              Limpar filtro
+            </SecondaryButton>
+          </div>
         </div>
 
         {products?.length > 0 ? (
