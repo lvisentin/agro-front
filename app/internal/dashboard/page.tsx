e73beed8182b78c5edbe4742bbe54eb72b4233e6
Gabriel Cavalcante <ggc.gabrielg@hotmail.com>
2023-11-22 21:18:43 -0300

    feat: adding new api call and selects handles

--- app/internal/dashboard/page.tsx
+++ app/internal/dashboard/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
 import InfoCard from '@/components/InfoCard/InfoCard';
+import PieChart from '@/components/PieChart/PieChart';
+import SelectField from '@/components/SelectField/SelectField';
 import { GetAnalyticsQuery } from '@/shared/graphql/queries/GetAnalytics.query';
+import { GetPlotsQuery } from '@/shared/graphql/queries/GetPlots.query';
+import { GetPropertiesQuery } from '@/shared/graphql/queries/GetProperties.query';
 import AnimatedPage from '@/shared/templates/AnimatedPage';
 import { useQuery } from '@apollo/client';
 import { useEffect, useState } from 'react';
@@ -15,7 +19,16 @@ export default function Home() {
     data: { analyticsDashboard } = {},
   } = useQuery(GetAnalyticsQuery);
 
+  const { loading: getPropertiesLoading, data: { properties } = {} } =
+    useQuery(GetPropertiesQuery);
+
+  const { loading: getPlotsLoading, data: { plots } = {}, refetch: refetchPlots} = 
+    useQuery(GetPlotsQuery);
+    
   const [userData, setUserData] = useState<any>(null);
+  const [selectedProperty, setSelectedProperty] = useState(0);
+  const [selectedPlot, setSelectedPlot] = useState(0);
+
 
   useEffect(() => {
     const localData = JSON.parse(localStorage.getItem('userData')!);
@@ -23,6 +36,29 @@ export default function Home() {
       setUserData(localData);
     }
   }, []);
+
+  useEffect(() => {
+    if (!getPropertiesLoading && properties && properties.length > 0 && selectedProperty === 0) {
+      setSelectedProperty(properties[0].id);
+    }
+  }, [getPropertiesLoading, properties, selectedProperty]);
+
+  useEffect(() => {
+    if (!getPlotsLoading && plots && plots.length > 0 && selectedPlot === 0) {
+      setSelectedPlot(plots[0].id);
+    }
+  }, [getPlotsLoading, plots, selectedPlot]);
+
+  useEffect(() => {
+    if (!selectedProperty) {
+      refetchPlots({ propertyId: undefined });
+      return;
+    }
+
+    refetchPlots({
+      propertyId: Number(selectedProperty),
+    });
+  }, [selectedProperty, refetchPlots]);
 
   useEffect(() => {
     refetch();
@@ -38,36 +74,77 @@ export default function Home() {
 
   return (
     <AnimatedPage>
-      <main>
-        <h1 className="font-semibold text-4xl mb-4">Olá, {userData?.name}!</h1>
+      <div className="analytics_wrapper">
+       <div className="prose flex justify-between w-full max-w-full">
+          <h2 className="prose-h2">Olá, {userData?.name}!</h2>
+        </div>
+        <div className='filter'>
+          <div className='flex items-center gap-4'>
+            <SelectField
+              name="property"
+              options={properties}
+              value={selectedProperty}
+              onChange={(e) => {
+                setSelectedPlot(0)
+                setSelectedProperty(e.target.value);
+              }}
+              disabled={getPropertiesLoading}
+              placeholder="Selecione uma propriedade"
+              label="Filtrar por propriedade"
+            />
 
+            <SelectField
+              options={plots?.length > 0 ? plots : []}
+              value={selectedPlot}
+              onChange={(e) => {
+                setSelectedPlot(e.target.value);
+              }}
+              name="plotId"
+              disabled={!selectedProperty || getPlotsLoading || loading}
+              placeholder="Selecione um talhão"
+              label="Filtrar por talhão"
+            />
+          </div>
+        </div>
         <div className="grid lg:grid-cols-4 md:grid-cols-2 gap-4 p-1 mb-5">
-          <InfoCard
-            title="Operações no último mês"
-            text={analyticsDashboard?.operationsCount}
-            color="green"
-          />
-          <InfoCard
-            title="Talhões"
-            text={analyticsDashboard?.plotsCount}
-            color="green"
-          />
-          <InfoCard
-            title="Total gasto no último mês"
-            text={analyticsDashboard?.totalSpent.toLocaleString('pt-BR', {
-              style: 'currency',
-              currency: 'BRL',
-            })}
-            color="green"
-          />
+          {[
+            {
+              title: "Operações no último mês",
+              text: analyticsDashboard?.operationsCount,
+              color: "green",
+            },
+            {
+              title: "Custo por talhão",
+              text: analyticsDashboard?.plotsCount,
+              color: "green",
+            },
+            {
+              title: "Custo total / ha",
+              text: analyticsDashboard?.costPerHectare,
+              color: "green",
+            },
+            {
+              title: "Total gasto no último mês",
+              text: analyticsDashboard?.totalSpent?.toLocaleString('pt-BR', {
+                style: 'currency',
+                currency: 'BRL',
+              }),
+              color: "green",
+            },
+          ].map((item, index) => (
+            <InfoCard
+              key={index}
+              title={item.title}
+              text={item.text}
+              color={item.color}
+            />
+          ))}
         </div>
 
-        {/* <div className="grid xl:grid-cols-3 lg:grid-cols-2 gap-2 p-1 mb-5">
-          <PieChart title={'Operações'} />
-          <PieChart title={'Saídas'} />
-          <LineChart title={'Lucros30d'} />
-        </div> */}
-      </main>
+        <div className="grid xl:grid-cols-3 lg:grid-cols-2 gap-2 p-1 mb-5">
+          <PieChart title={'Talhão'} />
+        </div>
+      </div>
     </AnimatedPage>
   );
 }
